5e70f3021c092d39123e952cd301371abe15baf9
Chan-Young Lee <chan9908181@gmail.com>
2022-10-23 22:05:29 +0200

    add the TODOs

--- src/homework1/Homework1.java
+++ src/homework1/Homework1.java
@@ -6,7 +6,6 @@ import static homework1.MiniJava.*;
 public class Homework1 {
     public static void main(String[] args) {
         //TODO program the right way to have the same out put as Chan.
-        write(0);
 
     }
 }
---
+++ src/homework2/Homework2.java
@@ -0,0 +1,8 @@
+package homework2;
+
+public class Homework2 {
+    public static void main(String[] args) {
+        //TODO
+
+    }
+}
