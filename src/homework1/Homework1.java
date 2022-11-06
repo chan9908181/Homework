d1ba6f56d19f4e41a12e3d56524b384bf5f781e8
thehoonstudio <info@thehoonstudio.com>
2022-11-06 16:40:37 +0100

    Homework1 push

--- src/homework1/Homework1.java
+++ src/homework1/Homework1.java
@@ -6,6 +6,26 @@ import static homework1.MiniJava.*;
 public class Homework1 {
     public static void main(String[] args) {
         //TODO program the right way to have the same out put as Chan.
+        int x;
+        int y;
+        int result;
+
+        x = readInt("insert the first number");
+        y = readInt("insert the second number");
+
+        if(x<=0 || y<=0) {
+            write("no result");
+            return;
+        }
+
+
+       else while(x != y){
+            if(x < y)
+                y= y-x;
+            else
+                x = x-y;
+        write("result: "+ x);
+       }
 
     }
 }
